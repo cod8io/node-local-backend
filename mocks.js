1f343b8e1b6479817f67877fb62b38ec5a143441
Martin Masek <martinmasek@Martins-MacBook-Pro.local>
2020-11-08 11:40:51 +0100

    mock for jmc

--- mocks.js
+++ mocks.js
@@ -1,11 +1,13 @@
-const OPERATION_TYPE = {
-    PUTAWAY: "putaway",
-    PICK: "pick",
-    LOAD: "load",
-    REVERSE: "reverse",
-    MOVE: "move",
-    REPLACE: "replace"
+const OPERATION_BY_TYPE = {
+    PUTAWAY: "3",
+    PICK: "1",
+    LOAD: "4",
+    REVERSE: "2",
+    MOVE: "5",
+    REPLACE: "6"
 }
+
+
 
 const TaskState = {
     Available: "AVL",
@@ -100,6 +102,29 @@ const storageMock = [
         "COMPLETED_BY": "",
         "CREATED_BY": "",
         "OPERATING_COMPANY": "PMUSA"
+    },
+    {
+        "SAF_TASK_ID": "112222",
+        "SAF_TASK_TYPE_ID": OPERATION_BY_TYPE.PUTAWAY,
+        "SAF_STATUS_CODE": "avl",
+        "SAF_CHECKOUT_DATE": "",
+        "SAF_START_DATE": "",
+        "SAF_END_DATE": "",
+        "UPDATED_DATE": "",
+        "WH": "rcof",
+        "BATCH": "6XXY14210",
+        "QUANTITY": "3",
+        "DESTINATION_LOCATION": "MC/B",
+        "DELIVERY_DATE": "2020-20-2",
+        "UOM": "pm8",
+        "WHSE_BUILDING_CODE": "15",
+        "STO_NUMBER": "",
+        "SAP_REF_NUMBER": "808080880",
+        "BOL_NO": "",
+        "APPROVED_BY": "",
+        "COMPLETED_BY": "",
+        "CREATED_BY": "",
+        "OPERATING_COMPANY": "JMC"
     }
 ]
 
@@ -238,6 +263,50 @@ const subtaskMock = {
             "RAW_QR_DATA": ""
         },
     ],
+    "112222": [
+        {
+            "SAF_TASK_ITEM_ID": "112222-1",
+            "SAF_TASK_ID": "112222",
+            "SRC_BIN": "29A041",
+            "CONTAINER_ID": "",
+            "BATCH": "6XXY14210",
+            "DEST_BIN": "07A022",
+            "SCANNED_SRC_BIN": "",
+            "SCANNED_DEST_BIN": "",
+            "SCANNED_ID": "",
+            "SCANNED_BY": "",
+            "SCANNED_DATE": "",
+            "RAW_QR_DATA": ""
+        },
+        {
+            "SAF_TASK_ITEM_ID": "112222-2",
+            "SAF_TASK_ID": "112222",
+            "SRC_BIN": "29A041",
+            "CONTAINER_ID": "",
+            "BATCH": "6XXY14210",
+            "DEST_BIN": "07A022",
+            "SCANNED_SRC_BIN": "",
+            "SCANNED_DEST_BIN": "",
+            "SCANNED_ID": "",
+            "SCANNED_BY": "",
+            "SCANNED_DATE": "",
+            "RAW_QR_DATA": ""
+        },
+        {
+            "SAF_TASK_ITEM_ID": "112222-3",
+            "SAF_TASK_ID": "112222",
+            "SRC_BIN": "29A041",
+            "CONTAINER_ID": "",
+            "BATCH": "6XXY14210",
+            "DEST_BIN": "07A022",
+            "SCANNED_SRC_BIN": "",
+            "SCANNED_DEST_BIN": "",
+            "SCANNED_ID": "",
+            "SCANNED_BY": "",
+            "SCANNED_DATE": "",
+            "RAW_QR_DATA": ""
+        },
+    ]
 }
 
 module.exports = {
